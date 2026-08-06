959923f53b4eb9f1515e5129cb27066b93264306
dvddvddvddvdvd <sculen.id@gmail.com>
2026-08-06 09:01:11 +0700

    Remove accidental git command from pocketbase.ts

--- src/pocketbase.ts
+++ src/pocketbase.ts
@@ -2,4 +2,4 @@ import PocketBase from 'pocketbase';
 
 const pbUrl = import.meta.env.VITE_PB_URL || 'https://db.zizazu.id';
 
-export const pb = new PocketBase(pbUrl);git add .
+export const pb = new PocketBase(pbUrl);
