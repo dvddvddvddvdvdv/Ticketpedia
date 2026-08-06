6c247f1e36d22f35a050b083b15991c2a519f72e
dvddvddvddvdvd <sculen.id@gmail.com>
2026-08-06 09:23:42 +0700

    Update vite config inputs for all HTML pages

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import { resolve, dirname } from 'path';
 import { fileURLToPath } from 'url';
 
-// Safely recreate __dirname for ES Modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
