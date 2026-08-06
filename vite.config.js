c03d58a7f55b82e9da06d8fb40fc6218cad0dc27
dvddvddvddvdvd <sculen.id@gmail.com>
2026-08-06 08:53:57 +0700

    Fix __dirname ES module crash

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,10 @@
 import { defineConfig } from 'vite';
-import { resolve } from 'path';
+import { resolve, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+// Safely recreate __dirname for ES Modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig({
   build: {
